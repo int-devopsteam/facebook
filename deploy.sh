16076755760cfec79c43e34c61e597c61cd19d5c
siddiqui intiquab <siddiquiintiquab@gmail.com>
2024-08-14 10:29:23 +0530

    updated again ...

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
+this is cherry pick command
+updated again
 updated in master branch....
 this is a delpoyment scrpit contains deloy infromation
--- intu.sh
+++ intu.sh
@@ -1,3 +1,5 @@
+updated again
+this is cherry pick command 
 updated in master branch
 updated content in dev branch
 this is a new file of my namee in github...
